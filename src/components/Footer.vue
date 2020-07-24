9c76b59bdd66ad5be93859578b7a5ff0beaf66f3
ayush29 <ayushjaindl@gmail.com>
2020-07-24 18:38:19 +0530

    Footer Embed Section

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer>
+        <div>
+            Add Footer here 
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+footer div{
+    background-color: lightgray;
+}
+</style>
