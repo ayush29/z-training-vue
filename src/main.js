bf684e34fbf919dddf24ad2880643f7f7461c13d
ayush29 <ayushjaindl@gmail.com>
2020-08-11 14:49:34 +0530

    .

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import VueSession from 'vue-session'
 
 Vue.config.productionTip = false
-
-// var options = {
-//     persist: true
-// }
-
-// Vue.use(VueSession,options)
 
 new Vue({
   render: h => h(App),
